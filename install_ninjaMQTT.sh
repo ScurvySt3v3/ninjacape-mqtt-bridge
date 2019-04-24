389469180319ac2324606ae8f8b3c41bad8e21ab
ScurvySt3v3 <36612826+ScurvySt3v3@users.noreply.github.com>
2019-04-25 00:58:11 +0200

    Update install_ninjaMQTT.sh

--- install_ninjaMQTT.sh
+++ install_ninjaMQTT.sh
@@ -1,22 +1,5 @@
 #!/bin/bash
 echo
-echo "Activate uart1"
-echo
-sudo curl -o /usr/local/bin/enable_uart1.sh https://raw.githubusercontent.com/ScurvySt3v3/ninjacape-mqtt-bridge/master/Uart/enable_uart1.sh
-sudo chmod +x /usr/local/bin/enable_uart1.sh
-sudo curl -o /lib/systemd/enable_uart1.service https://raw.githubusercontent.com/ScurvySt3v3/ninjacape-mqtt-bridge/master/Uart/enable_uart1.service
-sudo ln /lib/systemd/enable_uart1.service /etc/systemd/system/enable_uart1.service
-sudo systemctl daemon-reload
-sudo systemctl enable enable_uart1.service
-sudo systemctl start enable_uart1.service
-echo
-echo
-echo "It Works!"
-echo
-config-pin -q 926 uart
-config-pin -q 924 uart
-echo
-echo
 echo "Install some dependencys"
 echo
 echo
@@ -29,6 +12,22 @@ echo
 echo
 sudo pip install pyserial
 sudo pip install paho-mqtt
+echo
+echo "Activate uart1"
+echo
+sudo curl -o /usr/local/bin/enable_uart1.sh https://raw.githubusercontent.com/ScurvySt3v3/ninjacape-mqtt-bridge/master/Uart/enable_uart1.sh
+sudo chmod +x /usr/local/bin/enable_uart1.sh
+sudo curl -o /lib/systemd/enable_uart1.service https://raw.githubusercontent.com/ScurvySt3v3/ninjacape-mqtt-bridge/master/Uart/enable_uart1.service
+sudo ln /lib/systemd/enable_uart1.service /etc/systemd/system/enable_uart1.service
+sudo systemctl daemon-reload
+sudo systemctl enable enable_uart1.service
+sudo systemctl start enable_uart1.service
+echo
+echo
+config-pin -q 926 uart
+config-pin -q 924 uart
+echo
+echo
 echo
 echo
 echo "Get ninjaMQTTBridge and make it daemon-ised"
