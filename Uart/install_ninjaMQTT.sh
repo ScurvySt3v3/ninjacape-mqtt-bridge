a33b774835d61dc3a90ffa112c845153eaa24d68
ScurvySt3v3 <36612826+ScurvySt3v3@users.noreply.github.com>
2019-04-25 00:31:25 +0200

    Update install_ninjaMQTT.sh

--- Uart/install_ninjaMQTT.sh
+++ Uart/install_ninjaMQTT.sh
@@ -17,33 +17,3 @@ config-pin -q 926 uart
 config-pin -q 924 uart
 echo
 echo
-echo "Install some dependencys"
-echo
-echo
-sudo apt-get update
-sudo apt-get install --yes python-pip curl mosquitto mosquitto-clients
-echo
-echo
-echo "Install pyserial and paho-mqtt"
-echo
-echo
-sudo pip install pyserial
-sudo pip install paho-mqtt
-echo
-echo
-echo "Get ninjaMQTTBridge and make it daemon-ised"
-echo
-echo
-sudo curl --create-dirs -o /opt/ninjaCapeSerialMQTTBridge/ninjaMQTTBridge.py https://raw.githubusercontent.com/ScurvySt3v3/ninjacape-mqtt-bridge/master/ninjaMQTTBridge.py
-sudo chmod +x /opt/ninjaCapeSerialMQTTBridge/ninjaMQTTBridge.py
-sudo curl -o /etc/init/mqttbridge.conf https://raw.githubusercontent.com/ScurvySt3v3/ninjacape-mqtt-bridge/master/mqttbridge.conf
-sudo curl -o /etc/init.d/ninjaMQTTBridge.sh https://raw.githubusercontent.com/ScurvySt3v3/ninjacape-mqtt-bridge/master/ninjaMQTTBridge.sh
-sudo chmod +x /etc/init.d/ninjaMQTTBridge.sh
-sudo update-rc.d ninjaMQTTBridge.sh defaults
-sudo /etc/init.d/ninjaMQTTBridge.sh start
-echo
-echo
-echo "Don't forget to change the MQTT in /opt/ninjaCapeSerialMQTTBridge/ninjaMQTTBridge.py"
-echo
-echo
-
